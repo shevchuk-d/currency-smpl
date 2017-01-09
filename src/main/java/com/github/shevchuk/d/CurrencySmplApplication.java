5317c2e67bf7fe62b37d54851106193bfbc8625e
shevchuk-d <authentic.d@gmail.com>
2017-01-10 00:55:11 +0200

    Initial commit

---
+++ src/main/java/com/github/shevchuk/d/CurrencySmplApplication.java
@@ -0,0 +1,12 @@
+package com.github.shevchuk.d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencySmplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencySmplApplication.class, args);
+	}
+}
