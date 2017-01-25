01ffb7234108227d89c017029bfd47e4fc20956c
dmsh0216 <dmitrii.shevchuk@NetCracker.com>
2017-01-25 18:05:31 +0200

    + separate chart and table

--- src/main/java/com/github/shevchuk/d/currency_new/service/CurrencyRESTGetter.java
+++ src/main/java/com/github/shevchuk/d/currency_new/service/CurrencyRESTGetter.java
@@ -3,13 +3,11 @@ package com.github.shevchuk.d.currency_new.service;
 import com.github.shevchuk.d.chart.model.Chart;
 import com.github.shevchuk.d.currency_new.model.Currency;
 
-import com.github.shevchuk.d.currency_new.model.CurrencyEntity;
 import com.github.shevchuk.d.currency_new.model.CurrencyObject;
 
 import org.joda.time.DateTime;
 import org.joda.time.Days;
 import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,16 +18,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Locale;
+import java.util.*;
 
 /**
  * Created by dmsh0216 on 11/01/2017.
@@ -38,6 +32,9 @@ import java.util.Locale;
 public class CurrencyRESTGetter {
     @Autowired
     public Chart chart;
+
+    @Autowired
+    public Chart table;
 
     public static Logger log = LoggerFactory.getLogger(CurrencyRESTGetter.class);
 
@@ -100,8 +97,16 @@ public class CurrencyRESTGetter {
     }
 
 
-    public  void readJsonForParameters(String url, DateTime fromDate, DateTime toDate, String baseCurrency, String targetCurrency) throws IOException {
+    public void readJsonForParameters(String url, DateTime fromDate, DateTime toDate, String baseCurrency, String targetCurrency) throws IOException {
         ArrayList<JSONObject> currencyFromDateForBase = new ArrayList<>();
+
+        ArrayList<JSONObject> currencyFromDateForBaseForTable = new ArrayList<>();
+        for (int i = 1; i <= 10; i++){
+            currencyFromDateForBaseForTable
+                    .add(readJsonFromUrl(url + new DateTime().minusDays(10).plusDays(i).toString(DateTimeFormat.forPattern("yyyy-MM-dd")) + "?base=" + baseCurrency));
+        }
+        table.setCoordinates(new ArrayList<>());
+
         int days = Days.daysBetween(fromDate, toDate).getDays() + 1;
         int omitedDeys = days < 100 ? 0 : days/100;
         for (int i = 0; i <= days; i++){
@@ -112,47 +117,53 @@ public class CurrencyRESTGetter {
             i += omitedDeys;
         }
         chart.setCoordinates(new ArrayList<>());
-        currencyFromDateForBase.forEach(j -> {
-            parseJsonAsCurrencies(j).forEach(currencyObject -> {
-                if (baseCurrency.equals(currencyObject.getBase()) &&
-                targetCurrency.equals(currencyObject.getTarget())
-                        ) {
-                    log.info(String.valueOf(currencyObject.getDateTime()));
-                    log.info(String.valueOf(currencyObject.getBase()));
-                    log.info(String.valueOf(currencyObject.getTarget()));
-                    ArrayList<String> arrayList = new ArrayList<>();
 
-                    DateTime jodaTime = DateTimeFormat.forPattern("yyyy-MM-dd").parseDateTime(currencyObject.getDateTime());
-                    int i = 1;
-                    String newDate = "new Date("
-                            + jodaTime.getYear() + ", "
-                            + (jodaTime.getMonthOfYear() - i) + ", "
-                            + jodaTime.getDayOfMonth() + ")";
-                    if (chart.getCoordinates().size() >= 1 && chart.getCoordinates().get(chart.getCoordinates().size() - 1).get(0).equals(newDate) ){
-                        newDate = "new Date("
-                                + jodaTime.getYear() + ", "
-                                + (jodaTime.getMonthOfYear() - i) + ", "
-                                + (jodaTime.getDayOfMonth() + 1) + ")";
-                    }
-                    if (chart.getCoordinates().size() >= 2 && chart.getCoordinates().get(chart.getCoordinates().size() - 2).get(0).equals(newDate) ){
-                        newDate = "new Date("
-                                + jodaTime.getYear() + ", "
-                                + (jodaTime.getMonthOfYear() - i) + ", "
-                                + (jodaTime.getDayOfMonth() + 2) + ")";
-                    }
-                    arrayList.add(newDate);
-                    DecimalFormat decimalFormat = new DecimalFormat("0", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
-                    decimalFormat.setMaximumFractionDigits(6);
-                    arrayList.add(String.valueOf(
-                            ( decimalFormat.format(((float)currencyObject.getRate() ) /  Float.parseFloat(currencyMultiplier))
-//                    new BigDecimal( ((float)currencyObject.getRate() ) /  Float.parseFloat(currencyMultiplier) ).toPlainString()
-                    )));
-                    chart.getCoordinates().add(arrayList);
-                    log.info(chart.getCoordinates().toString());
-                }
-            });
-        });
+        chart = parseToArrays(chart, currencyFromDateForBase, baseCurrency, targetCurrency);
+        table = parseToArrays(table, currencyFromDateForBaseForTable, baseCurrency, targetCurrency);
+
     }
 
 
+    public Chart parseToArrays(Chart chart, ArrayList<JSONObject> currencyFromDateForBase, String baseCurrency, String targetCurrency) {
+        currencyFromDateForBase.forEach(j -> parseJsonAsCurrencies(j)
+                .forEach(currencyObject -> {
+                    if (baseCurrency.equals(currencyObject.getBase()) &&
+                            targetCurrency.equals(currencyObject.getTarget())
+                            ) {
+                        log.info(String.valueOf(currencyObject.getDateTime()));
+                        log.info(String.valueOf(currencyObject.getBase()));
+                        log.info(String.valueOf(currencyObject.getTarget()));
+                        ArrayList<String> arrayList = new ArrayList<>();
+
+                        DateTime jodaTime = DateTimeFormat.forPattern("yyyy-MM-dd").parseDateTime(currencyObject.getDateTime());
+                        int i = 1;
+                        String newDate = "new Date("
+                                + jodaTime.getYear() + ", "
+                                + (jodaTime.getMonthOfYear() - i) + ", "
+                                + jodaTime.getDayOfMonth() + ")";
+                        if (chart.getCoordinates().size() >= 1 && chart.getCoordinates().get(chart.getCoordinates().size() - 1).get(0).equals(newDate) ){
+                            newDate = "new Date("
+                                    + jodaTime.getYear() + ", "
+                                    + (jodaTime.getMonthOfYear() - i) + ", "
+                                    + (jodaTime.getDayOfMonth() + 1) + ")";
+                        }
+                        if (chart.getCoordinates().size() >= 2 && chart.getCoordinates().get(chart.getCoordinates().size() - 2).get(0).equals(newDate) ){
+                            newDate = "new Date("
+                                    + jodaTime.getYear() + ", "
+                                    + (jodaTime.getMonthOfYear() - i) + ", "
+                                    + (jodaTime.getDayOfMonth() + 2) + ")";
+                        }
+                        arrayList.add(newDate);
+                        DecimalFormat decimalFormat = new DecimalFormat("0", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+                        decimalFormat.setMaximumFractionDigits(6);
+                        arrayList.add(String.valueOf(
+                                ( decimalFormat.format(((float)currencyObject.getRate() ) /  Float.parseFloat(currencyMultiplier))
+//                    new BigDecimal( ((float)currencyObject.getRate() ) /  Float.parseFloat(currencyMultiplier) ).toPlainString()
+                                )));
+                        chart.getCoordinates().add(arrayList);
+                        log.info(chart.getCoordinates().toString());
+                    }
+                }));
+        return chart;
+    }
 }
